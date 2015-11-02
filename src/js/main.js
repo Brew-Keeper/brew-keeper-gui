9f84c5253773f5d7331a2d9dd307054a769bd6fa
Shay <hallshay@gmail.com>
2015-11-02 13:59:43 -0500

    Add route for login page.

--- src/js/main.js
+++ src/js/main.js
@@ -17,6 +17,9 @@
         .when('/info', {
           templateUrl: 'partials/more-info.html'
         })
+        .when('/login', {
+          templateUrl: 'partials/login.html'
+        })
         .otherwise({
           redirectTo: '/404.html',
           templateUrl: 'partials/404.html'
