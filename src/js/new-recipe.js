4e45b86bc6602b31759d228a12b3d64e75747b7f
Shay <hallshay@gmail.com>
2015-11-02 18:01:33 -0500

    Reveal Add Step form when recipe form is submitted.

--- src/js/new-recipe.js
+++ src/js/new-recipe.js
@@ -1,12 +1,24 @@
 ;(function(){//IFEE
 
 angular.module('brewKeeper')
-    .controller('createNewRecipe', function($scope, $http){
-      $scope.recipe = { }//Might need to prepopulate this with empty strings for each key... Maybe...
-      $scope.submit=function(){
-        $http.post('urlendpoint', $scope.recipe);//ADD ACTUAL ENDPOINT HERE!
-      $scope.recipe= { };
+  .controller('createNewRecipe', function($scope, $http){
+    $scope.recipe = { }//Might need to prepopulate this with empty strings for each key... Maybe...
+    $scope.submit=function(){
+      $http.post('urlendpoint', $scope.recipe);//ADD ACTUAL ENDPOINT HERE!
+    $scope.recipe= { };
+  }
+  $('.addSteps').on('click', function() {
+    $('.addSteps').removeClass('hidden');
+  });//Reveal "Add Step" when new recipe form is submitted.
+  })//controller for creating new recipe
+  .controller('createNewStep', function($scope, $http){
+    $scope.step = { }//Might need to prepopulate this with empty strings for each key... Maybe...
+    $scope.submit=function(){
+      $http.post('urlendpoint', $scope.step);//ADD ACTUAL ENDPOINT HERE!
+      $scope.step= { };
     }
-    });
+  })//controller for creating new step
 
 })();//END IFEE
+
+/* Eventually, it would be good to have the submitted form's information show up in place of the blank form so that the add new steps will be filled out instead of a new recipe form.  Then, as each step is added, it's info will show alongside the recipe info. */
