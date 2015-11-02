fc32b019b386ff65373eb256d6ba3a4ab7718592
David <dmwhite@gmail.com>
2015-11-02 13:21:35 -0500

    add controller for recipe-list partial page

--- src/js/recipe-list.js
+++ src/js/recipe-list.js
@@ -1,10 +1,12 @@
 ;(function(){//IFEE
-//
-// angular.module('brewKeeper')
-//     .controller('MainController', function($scope, $route, $routeParams, $location){
-//     $scope.$route = $route;
-//     $scope.$location = $location;
-//     $scope.$routeParams = $routeParams;
-//     })
-//
+
+angular.module('brewKeeper')
+      .controller('recipeList', function($scope, $http){
+          $http.get('api/users/1/user1.json')
+            .then(function(response){
+              $scope.recipes = response.data.recipe_list;
+            })
+      })
+
+
 })();//END IFEE
