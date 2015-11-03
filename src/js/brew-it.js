1cc7d54d379f6a2479b97b10de577db1f697b1b8
David <dmwhite@gmail.com>
2015-11-03 11:46:24 -0500

    add logic to prevent starting timer again while its running

--- src/js/brew-it.js
+++ src/js/brew-it.js
@@ -11,15 +11,24 @@ angular.module('brewKeeper')
               $scope.notes = response.data.brewnotes;
               $scope.countdownVal = response.data.total_duration;
               // $scope.countdownVal = 10;
+              // $scope.timerRunning = false;
             })
+            var timerRunning = false
           //start brew function
           $scope.startBrew = function(id){
+            console.log(timerRunning)
+            if(timerRunning){
+              // console.log("dear god please stop")
+              return;
+            }
             console.log("starting brew");
             // $scope.$broadcast('timer-start');
             // $(".timer").addClass("red");
             $(".1").addClass("red")
             $('timer')[0].start();
             $('timer')[1].start();
+            timerRunning = true;
+            console.log(timerRunning);
           };
           $scope.stopBrew = function(){
             console.log("pause button pressed");
@@ -43,11 +52,12 @@ angular.module('brewKeeper')
             var nextId = id + 1;
             console.log($scope.steps.length)
             if(nextId > $scope.steps.length){
+              timerRunning = false;
               return
             }
             $("."+ nextId).addClass("red");
             $('timer')[nextId].start();
-          }
+          };
       }) // end brewIt controller
 
 })();//END Angular IIFE
