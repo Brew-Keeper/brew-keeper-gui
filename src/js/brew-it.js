eca40d789a5d600249dd044b70f38a9a85596368
David <dmwhite@gmail.com>
2015-11-03 10:57:24 -0500

    individual timers working now...big baby step

--- src/js/brew-it.js
+++ src/js/brew-it.js
@@ -9,27 +9,39 @@ angular.module('brewKeeper')
               $scope.detail = response.data;
               $scope.steps = response.data.steps;
               $scope.notes = response.data.brewnotes;
-              // $scope.countdownVal = response.data.total_duration;
-              $scope.countdownVal = 10;
+              $scope.countdownVal = response.data.total_duration;
+              // $scope.countdownVal = 10;
             })
           //start brew function
-          $scope.startBrew = function(){
+          $scope.startBrew = function(id){
             console.log("starting brew");
-            $scope.$broadcast('timer-start');
-            $(".timer").addClass("red")
+            // $scope.$broadcast('timer-start');
+            // $(".timer").addClass("red");
+            // $(".1").addClass("red")
+            $('timer')[0].start();
+            $('timer')[1].start();
           };
           $scope.stopBrew = function(){
             console.log("pause button pressed");
             $scope.$broadcast('timer-stop');
+            // $(".timer").removeClass("red");
           };
           $scope.resetBrew = function(timerTime){
             console.log("reset button pressed");
             $scope.$broadcast('timer-reset');
+            $(".timer").removeClass("red");
             // $scope.$broadcast('timer-start');
           };
-          $scope.finishBrew = function(){
+          $scope.finishBrew = function(id){
             console.log("Brew complete");
-            $(".timer").removeClass("red")
+            // $(".timer").removeClass("red");
+            console.log("id = " + id)
+            nextStep(id);
+            // $("."+id).removeClass("red");
+          };
+          nextStep = function(id){
+            var nextId = id + 1;
+            $('timer')[nextId].start();
           }
       }) // end brewIt controller
 
