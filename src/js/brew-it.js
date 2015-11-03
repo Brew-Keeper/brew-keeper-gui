7ad1852ece57f3ff05f65531f02beb773039e271
David <dmwhite@gmail.com>
2015-11-03 10:06:39 -0500

    timer turns red when running, black when stopped...baby steps

--- src/js/brew-it.js
+++ src/js/brew-it.js
@@ -9,17 +9,14 @@ angular.module('brewKeeper')
               $scope.detail = response.data;
               $scope.steps = response.data.steps;
               $scope.notes = response.data.brewnotes;
-              $scope.countdownVal = response.data.total_duration;
+              // $scope.countdownVal = response.data.total_duration;
+              $scope.countdownVal = 10;
             })
           //start brew function
-          // $scope.countdownVal = 140;
           $scope.startBrew = function(){
             console.log("starting brew");
-            // steps = $scope.steps.length;
-            // console.log("steps = " + steps);
-            // $scope.$broadcast('timer-clear')
-            // $scope.$broadcast('timer-add-cd-seconds', $scope.detail.total_duration)
             $scope.$broadcast('timer-start');
+            $(".timer").addClass("red")
           };
           $scope.stopBrew = function(){
             console.log("pause button pressed");
@@ -27,11 +24,13 @@ angular.module('brewKeeper')
           };
           $scope.resetBrew = function(timerTime){
             console.log("reset button pressed");
-            // console.log(timerTime);
-            // $scope.$broadcast('timer-stop');
             $scope.$broadcast('timer-reset');
-            // $scope.countdownVal = timerTime;
+            // $scope.$broadcast('timer-start');
           };
+          $scope.finishBrew = function(){
+            console.log("Brew complete");
+            $(".timer").removeClass("red")
+          }
       }) // end brewIt controller
 
 })();//END Angular IIFE
