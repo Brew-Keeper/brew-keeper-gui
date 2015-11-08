80cd70c3efbfeb1f9bf86acd41e7e34a0d325d93
David <dmwhite@gmail.com>
2015-11-08 14:52:20 -0500

    rating.js cleanup

--- src/js/rating.js
+++ src/js/rating.js
@@ -1,24 +1,5 @@
 ;(function(){//IIFE
   angular.module('brewKeeper')
-  .controller('StarCtrl', ['$scope', function ($scope) {
-    // var currentRating = $rootScope.recipe.rating;
-      // var currentRating = 4;
-      // console.log(currentRating)
-      // $scope.rating = 0;
-      // $scope.ratings = [{
-      //     current: currentRating,
-      //     max: 5
-      // }];
-      //
-      // $scope.getSelectedRating = function (rating) {
-      //     console.log("getSelectedRating function");
-      //     console.log(rating)
-      //     var username = $scope.username;
-      //     var id = $scope.id;
-      //     console.log(username);
-      //     console.log(id);
-      // }
-  }])
 
   .directive('starRating', function () {
       return {
@@ -39,7 +20,6 @@
               var updateStars = function () {
                   scope.stars = [];
                   for (var i = 0; i < scope.max; i++) {
-                      console.log("stars " + i)
                       scope.stars.push({
                           filled: i < scope.ratingValue
                       });
@@ -47,20 +27,14 @@
               };
 
               scope.toggle = function (index) {
-                  console.log("scope.toggle fired")
                   scope.ratingValue = index + 1;
                   scope.onRatingSelected({
                       rating: index + 1
                   });
-                  // var newRating = {"rating": scope.ratingValue}
               };
 
               scope.$watch('ratingValue', function (oldVal, newVal) {
-                  console.log("scope.$watch")
-                  console.log("oldVal " + oldVal)
-                  console.log("newVal " + newVal)
                   if (newVal >=0 ) {
-                      console.log("calling updateStars function")
                       updateStars();
                   }
               });
