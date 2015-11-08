747af703dda1442da75142253d0f0aae8254cda2
David <dmwhite@gmail.com>
2015-11-08 14:50:13 -0500

    recipe-detail.html cleanup

--- src/js/rating.js
+++ src/js/rating.js
@@ -26,6 +26,7 @@
           template: '<ul class="rating">' +
               '<li ng-repeat="star in stars" ng-class="star" ng-click="toggle($index)">' +
               '\u2605' +
+              // '\u2605' +
               '</li>' +
               '</ul>',
           scope: {
