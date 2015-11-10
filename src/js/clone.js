60dbd576d7672372e3dab10bf9eff5ef4e0e194c
David <dmwhite@gmail.com>
2015-11-09 21:19:04 -0500

    create clone.js

---
+++ src/js/clone.js
@@ -0,0 +1,11 @@
+;(function(){//IIFE
+  console.log("clone.js loaded")
+  angular.module('brewKeeper')
+    .controller('cloneController', function($scope, $http, $location){
+      console.log("cloneController engaged")
+
+    }) //end cloneController
+
+
+
+})();//END Angular IIFE
