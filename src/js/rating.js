c4a8793d34ee2f6d136fa80e37c870780b75532a
David <dmwhite@gmail.com>
2015-11-12 18:47:28 -0500

    add new directive to figure out how to disable clicking on stars

--- src/js/rating.js
+++ src/js/rating.js
@@ -42,5 +42,45 @@
       }
   })
 
+  .directive('showRating', function () {
+      return {
+          restrict: 'A',
+          template: '<ul class="rating">' +
+              '<li ng-repeat="star in stars" ng-class="star">' +
+              '\u2605' +
+              // '\u2605' +
+              '</li>' +
+              '</ul>',
+          scope: {
+              ratingValue: '=',
+              max: '=',
+              onRatingSelected: '&'
+          },
+          link: function (scope, elem, attrs) {
+
+              var updateStars = function () {
+                  scope.stars = [];
+                  for (var i = 0; i < scope.max; i++) {
+                      scope.stars.push({
+                          filled: i < scope.ratingValue
+                      });
+                  }
+              };
+
+              scope.toggle = function (index) {
+                  scope.ratingValue = index + 1;
+                  scope.onRatingSelected({
+                      rating: index + 1
+                  });
+              };
+
+              scope.$watch('ratingValue', function (oldVal, newVal) {
+                  if (newVal >=0 ) {
+                      updateStars();
+                  }
+              });
+          }
+      }
+  })
 
 })();//END Angular IIFE
