ff42d6e75a72f18837aad564ce05c8317fe7c6e1
David <dmwhite@gmail.com>
2015-11-12 17:52:42 -0500

    create public-detail.js and link it in index.html

---
+++ src/js/public-detail.js
@@ -0,0 +1,9 @@
+;(function(){//IFEE
+angular.module('brewKeeper')
+  .controller('publicDetail', function(){
+    console.log("publicDetail controller")
+
+
+
+  }) //end recipDetail controller
+})();//END Angular IFEE
