8c4b5dcac04304c1e40050f21bcd2c1c07d0af98
David <dmwhite@gmail.com>
2015-11-12 18:09:14 -0500

    comment out all un-needed functionality from public detail view

--- src/js/public-detail.js
+++ src/js/public-detail.js
@@ -1,7 +1,25 @@
 ;(function(){//IFEE
 angular.module('brewKeeper')
-  .controller('publicDetail', function(){
+  .controller('publicDetail', function($http, $scope, $rootScope, $routeParams){
     console.log("publicDetail controller")
+
+    var id = $routeParams.id;
+    $scope.id = $routeParams.id;
+    $(document).scrollTop(0);
+
+    $http.get('https://brew-keeper-api.herokuapp.com/api/users/public/recipes/' + id + "/")
+      .then(function(response){
+        $rootScope.detail = response.data;
+        $rootScope.steps = response.data.steps;
+        $rootScope.notes = response.data.brewnotes;
+        var currentRating = $rootScope.detail.rating;
+        $scope.rating = 0;
+        $scope.ratings = [{
+            current: currentRating,
+            max: 5
+        }];
+
+    }) //end http.get
 
 
 
