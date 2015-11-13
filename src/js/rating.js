2accc404a93bcc98e2782fd84a4c22d18ed98e58
David <dmwhite@gmail.com>
2015-11-12 20:37:46 -0500

    stars in public view can not be altered

--- src/js/rating.js
+++ src/js/rating.js
@@ -7,7 +7,7 @@
           template: '<ul class="rating">' +
               '<li ng-repeat="star in stars" ng-class="star" ng-click="toggle($index)">' +
               '\u2605' +
-              // '\u2605' +
+              // '&#9733' +
               '</li>' +
               '</ul>',
           scope: {
@@ -42,45 +42,45 @@
       }
   })
 
-  .directive('showRating', function () {
+  .directive('viewRating', function () {
       return {
           restrict: 'A',
           template: '<ul class="rating">' +
-              '<li ng-repeat="star in stars" ng-class="star">' +
+              '<li ng-repeat="star in stars" ng-class="star" ng-click="toggle($index)">' +
               '\u2605' +
-              // '\u2605' +
+              // '&#9733' +
               '</li>' +
               '</ul>',
           scope: {
               ratingValue: '=',
               max: '=',
-              onRatingSelected: '&'
+              // onRatingSelected: '&'
           },
           link: function (scope, elem, attrs) {
-
-              var updateStars = function () {
+              // var updateStars = function () {
                   scope.stars = [];
                   for (var i = 0; i < scope.max; i++) {
                       scope.stars.push({
                           filled: i < scope.ratingValue
                       });
-                  }
+                  // }
               };
 
-              scope.toggle = function (index) {
-                  scope.ratingValue = index + 1;
-                  scope.onRatingSelected({
-                      rating: index + 1
-                  });
-              };
+              // scope.toggle = function (index) {
+              //     scope.ratingValue = index + 1;
+              //     scope.onRatingSelected({
+              //         rating: index + 1
+              //     });
+              // };
 
-              scope.$watch('ratingValue', function (oldVal, newVal) {
-                  if (newVal >=0 ) {
-                      updateStars();
-                  }
-              });
+              // scope.$watch('ratingValue', function (oldVal, newVal) {
+              //     if (newVal >=0 ) {
+              //         updateStars();
+              //     }
+              // });
           }
       }
   })
+
 
 })();//END Angular IIFE
