b614d9b51f2e5756b5d3b2c3e4449dbdbc6c3afb
David <dmwhite@gmail.com>
2015-11-16 13:35:24 -0500

    fixed show/hide of login/register/reset forms

--- src/js/login.js
+++ src/js/login.js
@@ -59,14 +59,11 @@ angular.module('brewKeeper')
       })//responses for bad login attempts
     }//submit function
     $('.show-signup').on('click', function(){
-      $('.register').removeClass('hidden');
+      $('section.register').removeClass('hidden');
       $('form.login').addClass('hidden')
     })//show signup form
 
-    $(".login-link").click(function(){
-      $('.register').addClass('hidden');
-      $('form.login').removeClass('hidden')
-    })
+    
   })//END CONTROLLER FOR LOGIN
 
 
@@ -126,9 +123,17 @@ angular.module('brewKeeper')
     } //end submitChangePassword function
 
     $('.show-reset').on('click', function(){
+      console.log("reset password button pressed")
       $('.reset-password').removeClass('hidden');
       $('form.login').addClass('hidden')
     });
+
+    $(".login-link").click(function(){
+      console.log("login link from change password controller")
+      $('section.register').addClass('hidden');
+      $('form.login').removeClass('hidden');
+      $('section.reset-password').addClass('hidden');
+    })
 
     $scope.requestReset = function(users){
       $scope.resetError = false;
