c4c423d802114c829b8887a5a52fce3373f6f4c0
Adam J Hartz <ahartz1@gmail.com>
2017-06-21 23:58:20 -0400

    Major refactor of brew-it.js; next move is to rename it
    Refactored to follow Angular 1 style guide
    https://github.com/johnpapa/angular-styleguide/tree/master/a1

--- src/js/brew-it.js
+++ src/js/brew-it.js
@@ -1,148 +1,198 @@
-;(function(){//IIFE
+;(function() {  //IIFE
+  'use strict';
 
-angular.module('brewKeeper')
-      .controller('brewIt', function($scope, $http, $routeParams, $location, $route, $rootScope){
-        $(document).scrollTop(0);
-        var id = $routeParams.id;
-        var username =$routeParams.username;
-        $scope.username = $routeParams.username;
-        $scope.showStars = false;
+  angular
+    .module('brewKeeper')
+    .controller('BrewIt', BrewIt);
 
-        $http.get($rootScope.baseUrl + '/api/users/' + username + '/recipes/' + id + '/')
-          .then(function(response){
-            $scope.detail = response.data;
-            $scope.steps = response.data.steps;
-            $scope.notes = response.data.brewnotes;
-            $scope.countdownVal = response.data.total_duration;
-            $scope.ratings = [{
-                max: 5
-            }];
-            var stepArray = []; //create an array of step #'s'
-            for(var step in $scope.steps){
-              stepArray.push($scope.steps[step].step_number);
-            }
-            $scope.stepArray = stepArray;
-            $scope.stepTotal = stepArray.length;
-          });
+  BrewIt.$inject = ['$scope', '$http', '$routeParams', '$rootScope'];
+  
+  function BrewIt($scope, $http, $routeParams, $rootScope) {
+    var vm = this;
+    vm.addBrewNote = addBrewNote;
+    vm.brewnote = {};
+    vm.nextStep = nextStep;
+    vm.rateRecipe = rateRecipe;
+    vm.reStartBrew = reStartBrew;
+    vm.resetBrew = resetBrew;
+    vm.showStars = false;
+    vm.startBrew = startBrew;
+    vm.username = $routeParams.username;
 
-        var stepArray = []; //create an array of step #'s'
-        for(var step in $rootScope.steps){
-          stepArray.push($rootScope.steps[step].step_number);
-        }
-        $rootScope.stepArray = stepArray;
-        $scope.stepTotal = stepArray.length;
+    ////////////////////////////////////////////////////////////////////////////
+    // INITIALIZATION /////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////
 
-        var timerRunning = false; //logic for brew timer
-        //start brew function
-        $scope.startBrew = function(brewCount){
-          if(timerRunning){
-            return;
-          }
-          $("."+$scope.stepArray[0]).removeClass("inactive-step").addClass("current-step");
-          $("div.delay").addClass("hidden").addClass("inactive-step");
-          $(".delay timer").addClass("hidden");
-          $(".time-" + $scope.countdownVal).addClass("timeline");//timeline
-          $("timer."+$scope.stepArray[0]).removeClass("hidden");//Show's timer for active step
-          $('timer')[1].start();
-          timerRunning = true;
-        };
+    var timerRunning = false;
+    var recipeUrl = $rootScope.baseUrl + '/api/users/' + $routeParams.username + '/recipes/' + $routeParams.id + '/';
+    var brewNoteUrl = recipeUrl + 'brewnotes/';
 
-        //runs when brew again button pushed
-        $scope.reStartBrew = function(){
-          if(timerRunning){
-            return;
-          }
-          $(".countdown").removeClass("hidden");
-          $(".brew-form").addClass("hidden");
-          $("div.restart").addClass("hidden");
-          $(".countdown").removeClass("hidden");
-          $("timer.delay").removeClass("hidden");
-          $(".step").addClass("inactive-step");
-          $("a[href].restart-brew").addClass("hidden");
-          $("a[href].add-brew-note").addClass("hidden");
-          $("a[href].reset-brew").removeClass("hidden");
-          $(".time-" + $scope.countdownVal).removeClass("timeline");//timeline
-          $('timer')[0].start();
-          $scope.showStars = false;
-        };
+    initialize();
+    initBrewNoteListeners();
 
-        $scope.resetBrew = function(){
+    ////////////////////////////////////////////////////////////////////////////
+    // FUNCTIONS //////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////
 
-          $(".current-step").addClass("hidden");
-          $("timer.counter").addClass("hidden");
-          $(".delay.hidden").removeClass("hidden");
-          $(".current-step").removeClass("current-step");
-          $(".countdown").removeClass("hidden");
-          $("div.countdown").addClass("hidden");
-          $(".time-" + $scope.countdownVal).removeClass("timeline");//timeline
-          $scope.$broadcast('timer-reset');
-          $("a[href].restart-brew").removeClass("hidden");
-          $("a[href].add-brew-note").removeClass("hidden");
-          // $(".rating").removeClass("hidden");
-          $("a[href].reset-brew").addClass("hidden");
-          $scope.showStars = true;
-          timerRunning = false;
+    /**
+     * Add a brew note to the recipe.
+     */
+    function addBrewNote() {
+      $http.post(brewNoteUrl, vm.brewnote);
+      // Prepare for adding another brew note
+      vm.brewnote = {};
+    }
 
-          //getting the data again solves the timers not
-          //resetting correctly
-          $http.get($rootScope.baseUrl + '/api/users/' + username + '/recipes/' + id + '/')
-            .then(function(response){
-              $scope.detail = response.data;
-              $scope.steps = response.data.steps;
-              $scope.notes = response.data.brewnotes;
-              $scope.countdownVal = response.data.total_duration;
-            });
-        }; //end resetBrew
-
-
-        $scope.nextStep = function(stepNumber, brewCount){
-          var nextStepIndex = $scope.stepArray.indexOf(stepNumber) + 1;
-          var nextStep = $scope.stepArray[nextStepIndex];
-          var prevStep = $scope.stepArray[nextStepIndex - 2];
-          var nextTimerId = $scope.stepArray.indexOf(stepNumber) + 2;
-          if(nextStepIndex >= $scope.steps.length){
-            //end of brew countdown
-            $scope.resetBrew();
-            //update brew_count
-            var recipe = {};
-            recipe.brew_count = brewCount + 1;
-            // brewCount++; //increment brew counter
-            $http.patch($rootScope.baseUrl + '/api/users/' + username + '/recipes/' + id + '/', recipe);
-            return;
-          }
-          $("timer."+stepNumber).addClass("hidden");// Hide last step
-          $("."+stepNumber).addClass("finished").removeClass("current-step");// Hide last step
-//Change next step to green  and grow next step to 200
-          $("timer."+nextStep).removeClass("hidden");
-          $("."+nextStep).addClass("current-step").removeClass("inactive-step");
-
-          $('timer')[nextTimerId].start();
-        }; //end nextStep function
-
-      $scope.brewnote = { };
-      $scope.addBrewNote=function(){
-        $http.post($rootScope.baseUrl + '/api/users/' + username + '/recipes/' + id + '/brewnotes/', $scope.brewnote)
-        .success(function (data) {
-          var id = data.id;
-        });
-      $scope.brewnote = { };
-      }; //Add Brew Note Form
-
+    /**
+     * Initialize listeners required for adding a brew note.
+     */
+    function initBrewNoteListeners() {
+      // Prepare to enter text into the brew note form
       $(".add-brew-note").on('click', function() {
         $(".brew-form").toggleClass("hidden");
         $('.input-focus').focus();
       });
-      $(".save-note").on('click', function() {
-        $(".brew-form").addClass("hidden");
-      });//Add hidden class to brewNote form on submit
-      $(".cancel-note").on('click', function() {
-        $(".brew-form").addClass("hidden");
-      });//Cancel BrewNote form
 
-      $scope.rateRecipe = function (rating, id) {
-        var newRating = {"rating": rating};
-        $http.patch($rootScope.baseUrl + '/api/users/' + username + '/recipes/' + id + '/', newRating);
-      };
+      function hideBrewNoteForm() {
+        $(".brew-form").addClass("hidden");
+      }
 
-    }); //end brewit controller
-})();//END Angular IIFE
+      // Hide brew note form on submit
+      $(".save-note").on('click', hideBrewNoteForm);
+
+      // Hide brew note form on cancel
+      $(".cancel-note").on('click', hideBrewNoteForm);
+    }
+
+    /**
+     * Gather the required data for this page.
+     */
+    function initialize() {
+      $(document).scrollTop(0);
+      $http.get(recipeUrl)
+        .then(function(response) {
+          vm.detail = response.data;
+          vm.steps = response.data.steps;
+          vm.notes = response.data.brewnotes;
+          vm.countdownVal = response.data.total_duration;
+          vm.ratings = [{
+              max: 5
+          }];
+          var stepArray = []; //create an array of step #'s'
+          for(var step in vm.steps){
+            stepArray.push(vm.steps[step].step_number);
+          }
+          vm.stepArray = stepArray;
+          vm.stepTotal = stepArray.length;
+        });
+    }
+
+    /**
+     * Advance the running brew timer to the next step.
+     */
+    function nextStep(stepNumber, brewCount) {
+      var nextStepIndex = vm.stepArray.indexOf(stepNumber) + 1;
+      var nextStep = vm.stepArray[nextStepIndex];
+      var prevStep = vm.stepArray[nextStepIndex - 2];
+      var nextTimerId = vm.stepArray.indexOf(stepNumber) + 2;
+      if (nextStepIndex >= vm.steps.length) {
+        // End of brew countdown
+        vm.resetBrew();
+        // Update brew_count
+        var recipe = {};
+        recipe.brew_count = brewCount + 1;
+
+        // TODO: Once we are no longer re-acquiring the recipe on reset, make
+        // sure we handle brewCount properly. The below increment was commented
+        // out in the original.
+
+        // brewCount++;
+
+        // Update the database with the new brew count
+        $http.patch(recipeUrl, recipe);
+        return;
+      }
+      $("timer."+stepNumber).addClass("hidden");// Hide last step
+      $("."+stepNumber).addClass("finished").removeClass("current-step");// Hide last step
+      // Change next step to green and grow next step to 200
+      $("timer."+nextStep).removeClass("hidden");
+      $("."+nextStep).addClass("current-step").removeClass("inactive-step");
+
+      $('timer')[nextTimerId].start();
+    }
+
+    /**
+     * Update the recipe with a new rating.
+     */
+    function rateRecipe(rating, id) {
+      var newRating = {"rating": rating};
+      $http.patch(recipeUrl, newRating);
+    }
+
+    /**
+     * Restart the brew timer, linked to the "Restart Brew" button.
+     */
+    function reStartBrew() {
+      if(timerRunning){
+        return;
+      }
+      $(".countdown").removeClass("hidden");
+      $(".brew-form").addClass("hidden");
+      $("div.restart").addClass("hidden");
+      $(".countdown").removeClass("hidden");
+      $("timer.delay").removeClass("hidden");
+      $(".step").addClass("inactive-step");
+      $("a[href].restart-brew").addClass("hidden");
+      $("a[href].add-brew-note").addClass("hidden");
+      $("a[href].reset-brew").removeClass("hidden");
+      $(".time-" + vm.countdownVal).removeClass("timeline");//timeline
+      $('timer')[0].start();
+      vm.showStars = false;
+    }
+
+    /**
+     * Reset when a timer is already running (to a non-running timer).
+     */
+    function resetBrew() {
+      $(".current-step").addClass("hidden");
+      $("timer.counter").addClass("hidden");
+      $(".delay.hidden").removeClass("hidden");
+      $(".current-step").removeClass("current-step");
+      $(".countdown").removeClass("hidden");
+      $("div.countdown").addClass("hidden");
+      $(".time-" + vm.countdownVal).removeClass("timeline");//timeline
+      $scope.$broadcast('timer-reset');
+      $("a[href].restart-brew").removeClass("hidden");
+      $("a[href].add-brew-note").removeClass("hidden");
+      $("a[href].reset-brew").addClass("hidden");
+      vm.showStars = true;
+      timerRunning = false;
+
+      // FIXME: Getting the data again solves the timers not resetting
+      // correctly, but we should not make a needless API call!
+      $http.get(recipeUrl)
+        .then(function(response){
+          vm.detail = response.data;
+          vm.steps = response.data.steps;
+          vm.notes = response.data.brewnotes;
+          vm.countdownVal = response.data.total_duration;
+        });
+    }
+
+    /**
+     * Start the brew timer.
+     */
+    function startBrew() {
+      if (timerRunning) {
+        return;
+      }
+      $("." + vm.stepArray[0]).removeClass("inactive-step").addClass("current-step");
+      $("div.delay").addClass("hidden").addClass("inactive-step");
+      $(".delay timer").addClass("hidden");
+      $(".time-" + vm.countdownVal).addClass("timeline");  //timeline
+      $("timer." + vm.stepArray[0]).removeClass("hidden");  //Show's timer for active step
+      $('timer')[1].start();
+      timerRunning = true;
+    }
+  }
+})();
