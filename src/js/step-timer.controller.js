95bbd8684964211eed1a0bbcfc3d54d8b1c8c6bf
Adam J Hartz <ahartz1@gmail.com>
2017-06-24 22:54:18 -0400

    Add moment to bower, add step-timer.controller.js
    Upon reading about angular.element(xxxxx).scope() not being available
    when debug is off, I had to devise a new solution to notifying the
    correct controller when to fire.
    
    The StepTimer controller defines functions that handle updating the
    BrewIt controller's `timerRunning` variable and only broadcast the
    timer's corresponding signals when appropriate (see timerStart).
    
    The signals listened for here are broadcast by the parent BrewIt
    controller.

---
+++ src/js/step-timer.controller.js
@@ -0,0 +1,60 @@
+;(function() {  //IIFE
+  'use strict';
+
+  angular
+    .module('brewKeeper')
+    .controller('StepTimer', StepTimer);
+
+  StepTimer.$inject = ['$scope'];
+
+  function StepTimer($scope) {
+    var stepVm = this;
+    stepVm.initTimer = initTimer;
+    stepVm.resetTimer = resetTimer;
+    stepVm.startTimer = startTimer;
+
+    ////////////////////////////////////////////////////////////////////////////
+    // LISTENERS //////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////
+
+    $scope.$on('resetTimer', resetTimer);
+
+    $scope.$on('startTimer', startTimer);
+
+    ////////////////////////////////////////////////////////////////////////////
+    // FUNCTIONS //////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Record which the step to which this timer belongs.
+     */
+    function initTimer(stepId) {
+      stepVm.stepId = stepId;
+      $scope.$broadcast('timer-reset');
+
+      // The countdown timer auto-starts on page load
+      if (stepId === 'countdown') {
+        $scope.$broadcast('timer-start');
+      }
+    }
+
+    /**
+     * Stop, then reset the timer.
+     */
+    function resetTimer(e) {
+      $scope.$broadcast('timer-stop');
+      $scope.$broadcast('timer-reset');
+      e.targetScope.vm.timerRunning = false;
+    }
+
+    /**
+     * Start the timer.
+     */
+    function startTimer(e, intendedRecipient) {
+      if (stepVm.stepId === intendedRecipient) {
+        $scope.$broadcast('timer-start');
+        e.targetScope.vm.timerRunning = true;
+      }
+    }
+  }
+})();
